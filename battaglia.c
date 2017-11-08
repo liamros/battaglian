ca2035f83e33f80d161494b6c95eee539edc82b2
Liam <liam.rossi199@gmail.com>
2017-11-08 19:19:12 +0100

    la funz va in loop infinito

--- battaglia.c
+++ battaglia.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int gio[13][13], pc[13][13];
+int gio[13][13], pc[13][13], i, j, k;
+
+int navig(int g[13][13], int i, int j, int k);
 
 int main ()
 {
@@ -12,4 +14,76 @@ int main ()
 			pc[i][j] = 0;
 		}
 	}
+//	while(k!=4)
+//	{
+		printf("Posizione nave da 3 posizioni\n");
+		printf("Inserisci riga (da 1 a 12) = ");
+		scanf("%d", &i);	
+		printf("Inserisci colonna (da 1 a 12) = ");
+		scanf("%d", &j);	
+		k = navig(gio[13][13], i, j, k);
+//	}
+}
+
+int navig(int gio[13][13], int i, int j, int k)
+{
+	char x;
+	errore : x = 0;
+	printf("Posizionare la nave verso su (S), giù (G), destra (d), o sinistra (s)? ");
+	scanf("%c", &x);
+	if (x != 'S' && x != 'G' && x != 'd' && x != 's')
+		goto errore;
+	if (x == 'S' && j-3 > 0)
+	{
+		gio[i][j] = 3;
+		j--;
+		gio[i][j] = 3;
+		j--;
+		gio[i][j] = 3;
+	}
+	else
+	{
+		printf("Impossibile\n");
+		goto errore;
+	}
+	if (x == 'G' && j+3 < 13)
+	{
+		gio[i][j] = 3;
+		j++;
+		gio[i][j] = 3;
+		j++;
+		gio[i][j] = 3;
+	}
+	else
+	{
+		printf("Impossibile\n");
+		goto errore;
+	}
+	if (x == 'd' && i+3 < 13)
+	{
+		gio[i][j] = 3;
+		i++;
+		gio[i][j] = 3;
+		i++;
+		gio[i][j] = 3;
+	}
+	else
+	{
+		printf("Impossibile\n");
+		goto errore;
+	}
+	if (x == 's' && i-3 > 0)
+	{
+		gio[i][j] = 3;
+		i--;
+		gio[i][j] = 3;
+		i--;
+		gio[i][j] = 3;
+	}
+	else
+	{
+		printf("Impossibile\n");
+		goto errore;
+	}
+	return k++;
 }
