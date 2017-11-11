10bec472c81f99898851704d044b26c008234321
Liam <liam.rossi199@gmail.com>
2017-11-11 02:18:40 +0100

    aggiunte condizioni per la sovrapposizione delle navi

--- battaglia.c
+++ battaglia.c
@@ -13,6 +13,7 @@ int main ()
 		scanf("%d", &i);	
 		printf("Inserisci colonna (da 1 a 12) = ");
 		scanf("%d", &j);	
+		putchar('\n');
 		k = navig(gio, i, j, k, v);
 		v++;					//v necessaria in navig
 	}
@@ -26,8 +27,14 @@ int main ()
 
 int navig(int gio[][13], int i, int j, int k, int v)
 {
+	if (v == 0)
+	{
+		gio[i][j] = k;
+		k++;
+		return k;
+	}
 	char x;
-	int const c = i, r = j; 	//sono necessarie altrimenti gli if cambiano ad ogni iterazione 
+	int const r = i, c = j; 	//sono necessarie altrimenti gli if cambiano ad ogni iterazione 
 	errore : x = 0;
 	printf("Posizionare la nave verso su (S), giù (G), destra (d), o sinistra (s)? ");
 	scanf("%c", &x);
@@ -36,31 +43,31 @@ int navig(int gio[][13], int i, int j, int k, int v)
 		goto errore;
 	for (int t = 0; t < k; t++)	//indica quante posiz occupano le navi
 	{
-		if (x == 's' && r-v > 0)  //r sostituisce j, perchè j avrà effetti collaterali
+		if((x == 's' && c-v > 0) && ((v == 1 && gio[r][c-1] == 0) || (v == 2 && gio[r][c-1] == 0 && gio[r][c-2] == 0)))
 		{
-			gio[i][j] = k;
+			gio[i][j] = k;  //r sostituisce j, perchè j avrà effetti collaterali
 			j--;
 		}
-			else if (x == 'd' && r+v < 13)	//la v verifica se la nave esce dalla mappa
-			{				//e varia in base al tipo di nave	
-				gio[i][j] = k;
-				j++;
+		else if((x == 'd' && c+v < 13) && ((v == 1 && gio[r][c+1] == 0) || (v == 2 && gio[r][c+1] == 0 && gio[r][c+2] == 0)))
+		{				//la v verifica se la nave esce dalla mappa
+			gio[i][j] = k;  	//e varia in base al tipo di nave	
+			j++;
+		}
+		else if((x == 'G' && r+v < 13) && ((v == 1 && gio[r+1][c] == 0) || (v == 2 && gio[r+1][c] == 0 && gio[r+2][c] == 0)))
+		{
+			gio[i][j] = k;
+			i++;
+		}
+		else if((x == 'S' && r-v > 0) && ((v == 1 && gio[r-1][c] == 0) || (v == 2 && gio[r-1][c] == 0 && gio[r-2][c] == 0)))
+		{
+			gio[i][j] = k;		//gli if controllano il carattere e se le navi si sovrappongono
+			i--;
+		}
+			else
+			{
+				printf("Impossibile\n\n");
+				goto errore;
 			}
-				else if (x == 'G' && c+v < 13)
-				{
-					gio[i][j] = k;
-					i++;
-				}
-					else if (x == 'S' && c-v > 0)
-					{
-						gio[i][j] = k;
-						i--;
-					}
-					else
-					{
-					printf("Impossibile\n\n");
-					goto errore;
-					}
 	}
 	k++;
 	return k;
